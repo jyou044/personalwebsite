685e6440d5ae6c41a43a3779147996631f6d4db5
JASON YOU <jasonyou026ix@gmail.com>
2021-05-30 19:52:06 -0400

    updated formatting

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,31 @@
-import React,{ Component } from 'react';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import './About.css';
-class About extends Component {
-    render() {
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+export default function About() {
+    const classes = useStyles();
         return (
+            
             <div>
-                test
+                <Grid container spacing ={3}>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>Hi, I'm Jason</Paper>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
-}
 
-export default About;
