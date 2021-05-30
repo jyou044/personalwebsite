cd2e869b9a8bb2e41b4a782c3b31a68ed6cea0f6
JASON YOU <jasonyou026ix@gmail.com>
2021-05-30 15:39:10 -0400

    adding files

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url:'Home',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'About',
+        url:'About',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Work Experience',
+        url:'WorkExperience',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Contact Info',
+        url:'ContactInfo',
+        cName: 'nav-links'
+    }, 
+]
