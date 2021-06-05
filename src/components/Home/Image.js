f7007e9b109de75ccbe940c21568257ac5c3bf64
JASON YOU <jasonyou026ix@gmail.com>
2021-06-05 15:40:46 -0400

    modified formatting of Home page

--- src/components/Home/Image.js
+++ src/components/Home/Image.js
@@ -2,13 +2,13 @@ import React from 'react';
 export default function Image() {
 return (
     <div>
-                <img 
-                    alt="newyorkportrait"
-                    height="300"
-                    width="cover"
-                    src={`${process.env.PUBLIC_URL}/me.jpg`} 
-                    title="New York Portrait" 
-                />
+            <img 
+                        alt="newyorkportrait"
+                        height="300"
+                        width="233"
+                        src={`${process.env.PUBLIC_URL}/me.jpg`} 
+                        title="New York Portrait" 
+                    />
     </div>
 );
 }
