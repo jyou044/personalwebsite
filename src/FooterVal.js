1da848e58be7a1f37c30462c069e1f5a90e2fc4b
JASON YOU <jasonyou026ix@gmail.com>
2021-06-05 22:57:57 -0400

    added elevation to footer

--- src/FooterVal.js
+++ src/FooterVal.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
-
+import Paper from '@material-ui/core/Paper';
 
 class FooterVal extends Component{
   render(){
     return (
     <div>
-      {this.props.children}
-
+      <Paper elevation={3}>
+        {this.props.children}
         <h2 className="white-text"><center>© 2021 Jason You</center></h2>
         <h2 className="grey-text text-lighten-4"><center>BETA V1.0</center></h2>
+      </Paper>
+      
     </div>
     );
   }
