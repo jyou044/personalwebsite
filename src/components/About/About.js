18108ea66e220c979e09bd6ca19cf8fd6b0ff889
JASON YOU <jasonyou026ix@gmail.com>
2021-06-10 18:52:56 -0400

    fixed para formatting

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -70,8 +70,8 @@ export default function About({ slides }) {
                         <Typography variant="h2" align="center" gutterBottom>I'm Jason</Typography>
                         <Typography variant="h2" align="center" className={classes.spacing} gutterBottom> </Typography>
                         <p> <b>I'm a 3rd year software engineering student at the University of Ottawa.
-                                From a young age, I have been fascinated with the field of Information Technology and it's applications. <br /> <br />As I made evident in my home page, I am an avid admirer of Steve Jobs and truly believe that simplicity is the ultimate sophistication!
-                                <br /><br />During my spare time, I'll tune into a Jays game or go outside with my Cannon DSLR camera to take some pictures. Below are some photos I have taken! 
+                                From a young age, I have been fascinated with the field of Information Technology and it's applications.  <br />As I made evident in my home page, I am an avid admirer of Steve Jobs and truly believe that simplicity is the ultimate sophistication!
+                               <br />During my spare time, I'll tune into a Jays game or go outside with my Cannon DSLR camera to take some pictures. Below are some photos I have taken! 
                             </b></p>
                     </Grid>
                     </Grid>
