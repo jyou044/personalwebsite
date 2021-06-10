73f6982a2c09447f209949d2b7a49ca0246ab886
JASON YOU <jasonyou026ix@gmail.com>
2021-06-10 19:00:01 -0400

    resolved conflicts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import './Home.css';
 import Image from '../Home/Image';
-import ReactPlayer from "react-player";
 const useStyles = makeStyles((theme) => ({
     textFormat: {
       padding: theme.spacing(2),
