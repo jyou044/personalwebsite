ce03ee75564e57d4135764ecf64eaec191e46e16
JASON YOU <jasonyou026ix@gmail.com>
2021-06-13 09:42:51 -0400

    made date dynamic

--- src/FooterVal.js
+++ src/FooterVal.js
@@ -3,12 +3,16 @@ import Paper from '@material-ui/core/Paper';
 import SocialFollow from '../src/components/About/SocialFollow';
 
 class FooterVal extends Component{
+  state = {
+    currentDateTime: new Date().getFullYear()
+
+  }
   render(){
     return (
     <div>
       <Paper elevation={3}>
         {this.props.children}
-        <h2 className="white-text"><center>© 2021 Jason You</center></h2>
+        <h2 className="white-text"><center>©  { this.state.currentDateTime } Jason You</center></h2>
         <h2 className="grey-text text-lighten-4"><center> V 1.1</center></h2>
         <SocialFollow />
       </Paper>
