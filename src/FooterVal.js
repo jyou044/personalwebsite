8f02124f5ddd4cda6e5f87842a47f18fffed1fe3
JASON YOU <jasonyou026ix@gmail.com>
2021-06-13 09:24:23 -0400

    added icons to footer

--- src/FooterVal.js
+++ src/FooterVal.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Paper from '@material-ui/core/Paper';
+import SocialFollow from '../src/components/About/SocialFollow';
 
 class FooterVal extends Component{
   render(){
@@ -9,6 +10,7 @@ class FooterVal extends Component{
         {this.props.children}
         <h2 className="white-text"><center>© 2021 Jason You</center></h2>
         <h2 className="grey-text text-lighten-4"><center> V 1.1</center></h2>
+        <SocialFollow />
       </Paper>
       
     </div>
