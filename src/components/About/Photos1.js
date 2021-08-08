bd0687758d7884d25ed81d06f1461ba7c48eca9d
JASON YOU <jasonyou026ix@gmail.com>
2021-08-08 16:48:05 -0400

    compressed images to improve performance

--- src/components/About/Photos1.js
+++ src/components/About/Photos1.js
@@ -3,42 +3,42 @@
 */
 export const Photos1 = [
     {
-        image: 'https://i.imgur.com/m4LUlfB.jpeg'
+        image: 'https://i.imgur.com/VQfyVUs.jpeg'
     },
     {
-        image: 'https://i.imgur.com/6kDvic0.jpeg'
+        image: 'https://i.imgur.com/0y0MfO2.jpeg'
     },
     {
-        image: 'https://i.imgur.com/YSuBywH.jpeg'
+        image: 'https://i.imgur.com/mFL1sOw.jpeg'
     },
     {
-        image: 'https://i.imgur.com/tyZ2fUV.jpeg'
+        image: 'https://i.imgur.com/WG77gCD.jpeg'
     },
     {
-        image: 'https://i.imgur.com/5cerBr5.jpeg'
+        image: 'https://i.imgur.com/qqc6l32.jpeg'
     },
     {
-        image: 'https://i.imgur.com/jL0frN7.jpeg'
+        image: 'https://i.imgur.com/w025qO7.jpeg'
     },
     {
-        image: 'https://i.imgur.com/6IAu30x.jpeg'
+        image: 'https://i.imgur.com/IDAWkz8.jpeg'
     },
     {
-        image: 'https://i.imgur.com/oOX4RuH.jpeg'
+        image: 'https://i.imgur.com/ll2SQtQ.jpeg'
     },
     {
-        image: 'https://i.imgur.com/HLZoSI2.jpg'
+        image: 'https://i.imgur.com/or3N7wD.jpeg'
     },
     {
-        image: 'https://i.imgur.com/JuB7zBv.jpg'
+        image: 'https://i.imgur.com/QTkAjWf.jpeg'
     },
     {
-        image: 'https://i.imgur.com/LKiqJXL.jpg'
+        image: 'https://i.imgur.com/5DWnoEa.jpeg'
     },
     {
-        image: 'https://i.imgur.com/hLNLRRv.jpg'
+        image: 'https://i.imgur.com/Xh2eizp.jpeg'
     },
     {
-        image: 'https://i.imgur.com/BeA8F3g.jpg'
+        image: 'https://i.imgur.com/990ieJ6.jpeg'
     },
 ];
